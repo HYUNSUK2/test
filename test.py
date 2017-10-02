c5831010227c02e19886e684d38827bb23e7c33a
HYUNSUK2 <ohs11244@naver.com>
2017-10-02 18:57:31 +0900

    Initial commit

---
+++ test.py
@@ -0,0 +1,12 @@
+import sys
+
+number = input('수를 입력하세요: ')
+if number.isdigit() is False:
+    print('정수를 입력하세요.')
+    sys.exit(0)
+
+number = int(number)
+if number % 3 == 0:
+    print('3의 배수입니다.')
+else:
+    print('3의 배수가 아닙니다.')
